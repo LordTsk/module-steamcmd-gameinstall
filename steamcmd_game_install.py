1efb1fd037fc11b522d7e114ab6a47998acfe22f
LordTSK <victor@hallynck.com>
2021-10-15 14:35:40 +0200

    test 2

--- steamcmd_game_install.py
+++ steamcmd_game_install.py
@@ -54,14 +54,12 @@ if pythonVersion == 3:
 elif pythonVersion == 2:
     from urllib import urlretrieve
 
-def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            game_number=dict(required=True, type='str'),
-            game_location_path=dict(required=True, type='str'),
-            steam_cmd_path=dict(required=True, type='str'),
-        )
-    )
+class SteamcmdException(Exception):
+    """
+    Base exception for the pysteamcmd package
+    """
+    pass
+
 
 class Steamcmd(object):
     def __init__(self, install_path):
@@ -166,11 +164,73 @@ class Steamcmd(object):
         except subprocess.CalledProcessError:
             raise SteamcmdException("Steamcmd was unable to run. Did you install your 32-bit libraries?")
 
+    def install_workshopfiles(self, gameid, workshop_id, game_install_dir, user='anonymous', password=None,
+                              validate=False):
+        """
+        Installs gamefiles for dedicated server. This can also be used to update the gameserver.
+        :param gameid: steam game id for the files downloaded
+        :param workshop_id: id of workshop item to download
+        :param game_install_dir: installation directory for gameserver files
+        :param user: steam username (defaults anonymous)
+        :param password: steam password (defaults None)
+        :param validate: should steamcmd validate the gameserver files (takes a while)
+        :return: subprocess call to steamcmd
+        """
+        if validate:
+            validate = 'validate'
+        else:
+            validate = None
+
+        steamcmd_params = (
+            self.steamcmd_exe,
+            '+login {} {}'.format(user, password),
+            '+force_install_dir {}'.format(game_install_dir),
+            '+workshop_download_item {} {}'.format(gameid, workshop_id),
+            '{}'.format(validate),
+            '+quit',
+        )
         try:
             return subprocess.check_call(steamcmd_params)
         except subprocess.CalledProcessError:
             raise SteamcmdException("Steamcmd was unable to run. Did you install your 32-bit libraries?")
 
+    def upload_workshopfiles(self, user, password, workshop_vdf_path=None, workshop_vdf=None):
+        """
+        Uploads workshop items in accordance to the workshop vdf file.
+        :param workshop_vdf_path: path to workshop vdf file.
+        :param workshop_vdf: Optional, vdf object See https://pypi.org/project/vdf/
+        :param user: steam username (required valid user)
+        :param password: steam password (required valid user password)
+        """
+        if workshop_vdf_path:
+            vdf_data = self._parse_vdf(vdf_path=workshop_vdf_path)
+        elif workshop_vdf:
+            vdf_data = self._parse_vdf_text(vdf_data=workshop_vdf)
+
+        with open("{install_path}/workshop.vdf".format(install_path=self.install_path), "w") as vdf_file:
+            vdf.dump(vdf_data, vdf_file)
+
+        # Upload the workshop files
+        steamcmd_params = (
+            self.steamcmd_exe,
+            '+login {} {}'.format(user, password),
+            '+workshop_build_item',
+            '{install_path}/workshop.vdf'.format(install_path=self.install_path),
+            '+quit',
+        )
+        try:
+            return subprocess.check_call(steamcmd_params)
+        except subprocess.CalledProcessError:
+            raise SteamcmdException("Steamcmd was unable to run. Did you install your 32-bit libraries?")
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            game_number=dict(required=True, type='str'),
+            game_location_path=dict(required=True, type='str'),
+            steam_cmd_path=dict(required=True, type='str'),
+        )
+    )
 
     game_number_local = module.params.get('game_number')
     game_location_path_local = module.params.get('game_location_path')
