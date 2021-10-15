ae55c307cca866036c21aa2785bff278b43fe462
LordTSK <victor@hallynck.com>
2021-10-15 11:38:11 +0200

    correction after ansible sanity check

--- steamcmd_game_install.py
+++ steamcmd_game_install.py
@@ -7,20 +7,19 @@ import pysteamcmd
 import os
 from ansible.module_utils.basic import AnsibleModule
 
-def main(): 
-    module = AnsibleModule( 
-        argument_spec=dict( 
-            game_number    = dict(required=True, type='str'), 
-            game_location_path    = dict(required=True, type='str'),
-            steam_cmd_path    = dict(required=True, type='str'), 
-        ) 
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            game_number = dict(required=True,type='str'), 
+            game_location_path = dict(required=True,type='str'),
+            steam_cmd_path = dict(required=True,type='str'), 
+        )
     )
 
     game_number_local = module.params.get('game_number')
     game_location_path_local = module.params.get('game_location_path')
     steam_cmd_path_local = module.params.get('steam_cmd')
-
-
     steamcmd = pysteamcmd.Steamcmd(steam_cmd_path_local)
     output_command = steamcmd.install_gamefiles(gameid=game_number_local, game_install_dir=game_location_path_local, user='anonymous', password=None, validate=True)
     output_command = str(output_command)
@@ -30,37 +29,37 @@ def main():
     else:
         resultat = 'Failed \n'+output_command
 
-    module.exit_json(changed=False, results=resultat) 
+    module.exit_json(changed=False,results = resultat)
 
 if __name__ == "__main__": 
     main()
 
 DOCUMENTATION='''
 module: steamcmd_game_install
-author: Victor 
+author: Victor
 description: This module install any steamcmd available game as anonymous on a server with preinstalled steamcmd
 
-options: 
-  game_number: 
+options:
+  game_number:
     description: Game ID in steamcmd https://developer.valvesoftware.com/wiki/Dedicated_Servers_List
-    required: yes 
-  game_location_path: 
+    required: yes
+  game_location_path:
     description: Where the game server file will be stored
-    required: yes 
-  steam_cmd: 
+    required: yes
+  steam_cmd:
     description: Location of the steamcmd binary
-    required: yes 
+    required: yes
 '''
 
 EXAMPLES='''
-- name: "CSGO Install" 
-  steamcmd_game_install: 
-    game_number: "740" 
+- name: "CSGO Install"
+  steamcmd_game_install:
+    game_number: "740"
     game_location: "/home/user-ansible/games/csgo"
-    steam_cmd: "/usr/game/steamcmd" 
+    steam_cmd: "/usr/game/steamcmd"
 '''
 
 RETURN = '''
-results: 
+results:
     description: return installation status
 '''
